87a2c02556d564698ab36c8c42435e421309cc69
1194314874@qq.com <1194314874@qq.com>
2026-04-01 21:47:56 +0800

    添加

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,6 +6,21 @@ const request = axios.create({
     timeout: 600000
 })
 
+// axios的请求的 request 拦截器 --获取localStore中的token,在请求头中增加token请求头
+request.interceptors.request.use(
+    (config) => {// 成功回调
+        // 在请求头中获取TOKEN
+        const data = JSON.parse(localStorage.getItem("loginUser"))
+        if (data && data.token) {
+            config.headers.authorization = "Bearer " + data.token
+        }
+        return config;
+    },
+    (error) => {// 失败回调
+        return Promise.reject(error)
+    }
+)
+
 // axios的响应 response 拦截器
 request.interceptors.response.use(
     (response) => {// 成功回调
